c7cda78b61671aceb34b0fe52b7219cdc1682f65
Palomar <calvino.palomar@gmail.com>
2019-09-11 21:15:07 +0500

    Add .csv tables and source code drafts

---
+++ payphone_hero/README.md
@@ -0,0 +1,15 @@
+# Таксофонный герой
+
+[Call to Home](https://py.checkio.org/ru/mission/calls-home//) из Checkio
+
+## Задача
+
+![cover image]()
+
+## I/O
+
+### Input / Дано:
+
+## Подготовка
+
+## Решение
--- payphone_hero/payphone_hero.py
+++ payphone_hero/payphone_hero.py
@@ -1,14 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# выбираем записи для каждого месяца по категории (id-должность-категория победителя)
+# считаем стоимость звонков
+# вычисляем процент стоимости звонков от зарплаты
+# у кого процент больше - тот победитель лотереи (если один участник - он и победитель)
+
+import csv
+from datetime import datetime
 from math import ceil
 
 
-def total_cost(calls):
+def read_phone_info(csv_name):
+
+    with open(csv_name, 'r') as phone_info:
+        reader = csv.reader(phone_info)
+        read_phone_info = [row for row in reader]
+    return read_phone_info
+
+
+def time_convert():
+
+    a = read_phone_info('payphonehero_data_phonetime.csv')
+    b = []
+
+    for i in range(1, len(a) - 1):
+        month = datetime.strptime(a[i][1], '%Y-%m-%d %H:%M:%S').strftime('%b %y')
+        date = a[i][1].split()[0]
+        start = datetime.strptime(a[i][1], '%Y-%m-%d %H:%M:%S')
+        end = datetime.strptime(a[i][2], '%Y-%m-%d %H:%M:%S')
+        sec = (end - start)
+        b.append([a[i][0], month, date, sec.seconds])
+    return sorted(b, key=lambda x:x[1])
+
+
+print(time_convert())
+
+
+def category():
+
+    a = read_phone_info('payphonehero_data_herocategory.csv')
+    b = {a[i][0]: a[i][1] for i in range(1, len(a) - 1)}
+
+    print(b)
+
+category()
+
+
+def payphone_hero(calls):
 
     day_dict = {}
 
     for i in calls:
-        date, time, sec = i.split(' ')
+        date, sec = i[1], i[2]
         minutes = ceil(int(sec) / 60)
-        print(date, time, sec, minutes)
+        print(date, sec, minutes)
         day_dict[date] = day_dict.get(date, 0) + minutes
-
     return sum((2 * price - min(price, 100)) for price in day_dict.values())
+
+
+#print(payphone_hero(time_convert()))
+#read_phone_info('payphonehero_data_phonetime.csv')
